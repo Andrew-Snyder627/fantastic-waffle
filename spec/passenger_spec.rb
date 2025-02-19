2dc5cc3c15fab592346f24c4f9143783eef2a639
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-19 11:13:25 -0500

    Adds tests for iteration 1

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -7,5 +7,42 @@ end
 
 # Your tests should start here!
 RSpec.describe Passenger do 
-    
+
+    it 'exists' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie).to be_a(Passenger)
+      expect(taylor).to be_a(Passenger)
+    end
+
+    it 'can assign attributes' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.name).to eq("Charlie")
+      expect(charlie.age).to eq(18)
+
+      expect(taylor.name).to eq("Taylor")
+      expect(taylor.age).to eq(12)
+    end
+
+    it 'can determine if passenger is adult' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.adult?).to be true
+      expect(taylor.adult?).to be false
+    end
+
+    it 'can determine if passenget is a driver' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.driver?).to be false
+
+      charlie.drive
+      
+      expect(charlie.driver?).to be true
+    end
 end
