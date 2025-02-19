f16da2e2c345c824227a1a5ea3b3eff62dd09209
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-19 12:38:34 -0500

    Pass test for iteration 3

---
+++ lib/park.rb
@@ -0,0 +1,33 @@
+class Park
+    attr_reader :name, :admission_price, :vehicles
+
+    def initialize(name, admission_price)
+        @name = name
+        @admission_price = admission_price
+        @vehicles = []
+    end
+
+    def admit_vehicle(vehicle)
+        @vehicles << vehicle
+    end
+
+    def all_passengers
+        passengers = []
+
+        @vehicles.each do |vehicle| # Could have used flatten/map but wanted to try the nested iteration
+            vehicle.passengers.each do |passenger|
+                passengers << passenger
+            end
+        end
+
+        passengers
+    end
+
+    def revenue
+        adults = all_passengers.select do |passenger| # Trying to use do/end notation for my enums from now on per Mike
+            passenger.adult?
+        end
+        
+        adults.count * @admission_price
+    end
+end
